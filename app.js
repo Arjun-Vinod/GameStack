ccdab9a150f06f06efe27c651149f8e0c98d9e17
Arjun-Vinod <arjun.vinodaj77@gmail.com>
2025-01-01 21:41:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express=require('express');
 const mongoose=require('mongoose');
 
 const app=express();
+const cors=require('cors');
 require('dotenv').config();
 mongoose.connect(process.env.DB_URI);
+const cors=require('cors');
 
 const itemrouter=require('./routes/route');
 app.use('/api',itemrouter);
@@ -14,12 +16,12 @@ app.set("view engine");
 app.set("views",path.join(__dirname,"views"))
 
 
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'views','index.html'))
 });
 
 
-const PORT=4000;
 app.listen(PORT,()=>{
-    console.log(`Server is running omn the port http://localhost:${PORT}`);
+    console.log(`Server is running omn the port ${PORT}`);
 })
